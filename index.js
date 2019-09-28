33f3c77e1b21924fac6f897efa51621aea4d569c
admin <admin@admins-MacBook-Pro.local>
2019-09-28 07:30:53 +0700

    ..

--- index.js
+++ index.js
@@ -24,55 +24,40 @@ fs.readdir(directoryPath, async (err, files) => {
     });
 
     if (fileArr.length > 0) {
-      const fetchResult = async () => {
-        let final = {};
+      const uyt = await fetchResult(fileArr);
 
-        await fileArr.map(async fileName => {
-          console.log(3);
-          const data = await i18nStringsFiles.readFile(
-            __dirname + "/public/vi.lproj/" + fileName,
-            "UTF-8"
-          );
-          console.log(data)
-          console.log(4);
-          const nameOfFile = fileName.substring(0, fileName.length - 8);
-
-          console.log(5)
-
-          await Object.keys(data).forEach(async key => {
-            console.log(6)
-            const newkey = nameOfFile + "." + key;
-            console.log(7)
-            final[newkey] = await data[key];
-            // delete data[key];
-
-            //   console.log(final);
-
-            //   fs.writeFile(
-            //     __dirname + "/models/vi.json",
-            //     JSON.stringify(final),
-            //     "utf8",
-            //     (err, data) => {
-            //       if (!err) {
-            //         console.log("Done!");
-            //       }
-            //     }
-            //   );
-          });
-        });
-
-        console.log(final)
-        return Promise.resolve(final);
-      };
-      console.log(2);
-      const uyt = await fetchResult();
-      console.log(8);
-      console.log(uyt);
-      // fetchResult()
-      //   .then(result => console.log(result))
-      //   .catch(e => console.log(e));
+      await fs.writeFile(
+        __dirname + "/models/vi.json",
+        JSON.stringify(uyt),
+        "utf8",
+        (err, data) => {
+          if (!err) {
+            console.log("Done!");
+          }
+        }
+      );
     }
   } catch (e) {
     console.log(e);
   }
 });
+
+const fetchResult = async fileArr => {
+  let final = {};
+
+  await fileArr.map(async fileName => {
+    const data = await i18nStringsFiles.readFileSync(
+      __dirname + "/public/vi.lproj/" + fileName,
+      "UTF-8"
+    );
+    const nameOfFile = fileName.substring(0, fileName.length - 8);
+
+    await Object.keys(data).forEach(async key => {
+      const newkey = nameOfFile + "." + key;
+      final[newkey] = await data[key];
+      // delete data[key];
+    });
+  });
+
+  return final;
+};
